3c6fbfa5057d9441c1d3bd95bcd9f9170d45fe8b
Eiman Farooq <eimanfarooq15@gmail.com>
2025-06-16 02:56:15 +0500

    custom my string and my vector added

--- OOP_FINAL_PROJECT/myvector.h
+++ OOP_FINAL_PROJECT/myvector.h
@@ -1,1 +1,47 @@
-#pragma once
+#ifndef MYVECTOR_H
+#define MYVECTOR_H
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class MyVector {
+    T* data;
+    int capacity;
+    int length;
+
+    void resize() {
+        int newCap = capacity * 2;
+        T* newData = new T[newCap];
+        for (int i = 0; i < length; ++i) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+        capacity = newCap;
+    }
+
+public:
+    MyVector() : capacity(10), length(0) {
+        data = new T[capacity];
+    }
+
+    void push_back(const T& value) {
+        if (length >= capacity) resize();
+        data[length++] = value;
+    }
+
+    int size() const {
+        return length;
+    }
+
+    T& operator[](int index) {
+        return data[index];
+    }
+
+    ~MyVector() {
+        delete[] data;
+    }
+};
+
+#endif
